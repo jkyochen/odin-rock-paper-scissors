d8e4dfa30b5a4a0cfe0e6267153fc6d7554064a1
ChenZiQiang <ziqiang.chen@outlook.com>
2023-07-27 23:20:03 +0900

    Add play 5 round game

--- script.js
+++ script.js
@@ -1,26 +1,60 @@
 
-const choices = ["Rock", "Paper", "Scissors"];
+const CHOICE = {
+    Rock: "Rock",
+    Paper: "Paper",
+    Scissors: "Scissors",
+};
+
+const PLAY_RESULT = {
+    Tie: "Tie",
+    Win: "Win",
+    Lose: "Lose",
+};
+
+const choices = [CHOICE.Rock, CHOICE.Paper, CHOICE.Scissors];
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-    if (!choices.includes(playerSelection)) {
-        return "error choice";
-    }
     if (playerSelection === computerSelection) {
-        return "Tie";
+        return PLAY_RESULT.Tie;
     }
-    if (playerSelection === "Rock" && computerSelection === "Scissors"
-        || playerSelection === "Paper" && computerSelection === "Rock"
-        || playerSelection === "Scissors" && computerSelection === "Paper") {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    if (playerSelection === CHOICE.Rock && computerSelection === CHOICE.Scissors
+        || playerSelection === CHOICE.Paper && computerSelection === CHOICE.Rock
+        || playerSelection === CHOICE.Scissors && computerSelection === CHOICE.Paper) {
+        return PLAY_RESULT.Win;
     }
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    return PLAY_RESULT.Lose;
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let playWinCount = 0;
+    let computerWinCount = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = window.prompt();
+        if (!playerSelection) {
+            break;
+        }
+        playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+        if (!choices.includes(playerSelection)) {
+            break;
+        }
+        const computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        if (result === PLAY_RESULT.Tie) {
+            i--;
+        } else if (result === PLAY_RESULT.Win) {
+            playWinCount++;
+        } else {
+            computerWinCount++;
+        }
+    }
+    alert(JSON.stringify({
+        playWinCount,
+        computerWinCount,
+    }));
+}
+
+game();
